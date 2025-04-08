30351da4cb25964c6d3bfc8e5312596f56d83d71
kim-yeonghyeon <yhkim@hiaas.co.kr>
2025-04-08 15:48:30 +0900

    Charts.js 전역 설정 추가

---
+++ src/app/config/chartjsConfig.js
@@ -0,0 +1,4 @@
+// src/app/config/chartjsSetup.js
+import { Chart, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+
+Chart.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@/shared/styles/theme.css'
 import './main.css'
+import '@/app/config/chartjsConfig.js'
 import App from './app/App'
 
 createRoot(document.getElementById('root')).render(
